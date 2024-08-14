02562eafa9b12b0fa045648b06635d1160bc2bb8
Sreegovind <sreegovind1011@gmail.com>
2024-08-14 09:37:06 +0530

    hbj

--- server/routes/Recipe/Generalroute.js
+++ server/routes/Recipe/Generalroute.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { Generalget } from "../../Controllers/Recipe/General/Generalget.js";
-import { Generalpost } from "../../controllers/Recipe/General/Generalpost.js";
-
+import { Generalpost } from "../../Controllers/Recipe/General/Generalpost.js";
 const router = express.Router();
 
 router.post("/", Generalpost);
