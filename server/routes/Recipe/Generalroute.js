1fe117c1cb6711ecc73e0a26a19ea17ccc2b908d
Sreegovind <sreegovind1011@gmail.com>
2024-08-14 09:32:41 +0530

    tas

--- server/routes/Recipe/Generalroute.js
+++ server/routes/Recipe/Generalroute.js
@@ -1,11 +1,10 @@
 import express from "express";
-import { Generalget } from "../../controllers/Recipe/General/Generalget.js";
+import { Generalget } from "../../Controllers/Recipe/General/Generalget.js";
 import { Generalpost } from "../../controllers/Recipe/General/Generalpost.js";
 
 const router = express.Router();
 
 router.post("/", Generalpost);
 router.get("/", Generalget);
-
 
 export default router;
